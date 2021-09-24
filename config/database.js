0fde52a788a633088d64412c1032b60ac2f1e327
Mikonuji <mtneugebauer1998@gmail.com>
2021-09-24 14:01:35 -0500

    Added the ability to add credits from strapi.

--- config/database.js
+++ config/database.js
@@ -5,14 +5,16 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         host: env('DATABASE_HOST', process.env.DATABASE_HOST),
-        srv: env.bool('DATABASE_SRV', false),
+        srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', process.env.DATABASE_NAME),
         username: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
         password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', 'nwc')
+        //authenticationDatabase: env('AUTHENTICATION_DATABASE', "nwc"), // move this into the .env!!!!
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool("DATABASE_SSL", true),
       },
     },
   },
