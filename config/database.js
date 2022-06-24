9b03c2708682db7f969417cc421dc8b415161a4a
gaguirr5 <77712470+gaguirr5@users.noreply.github.com>
2022-06-24 14:19:14 -0500

    update backend for postgres database

--- config/database.js
+++ config/database.js
@@ -2,18 +2,19 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: 'bookshelf',
       settings: {
+        client:'postgres',
         host: env('DATABASE_HOST', process.env.DATABASE_HOST),
-        srv: env.bool('DATABASE_SRV', process.env.DATABASE_SRV),
-        port: env.int('DATABASE_PORT', 27017),
+        // srv: env.bool('DATABASE_SRV', process.env.DATABASE_SRV),
+        port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', process.env.DATABASE_NAME),
         username: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
         password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', process.env.AUTHENTICATION_DATABASE),
-        ssl: env.bool('DATABASE_SSL', process.env.DATABASE_SSL),
+        // authenticationDatabase: env('AUTHENTICATION_DATABASE', process.env.AUTHENTICATION_DATABASE),
+        // ssl: env.bool('DATABASE_SSL', process.env.DATABASE_SSL),
       },
     },
   },
