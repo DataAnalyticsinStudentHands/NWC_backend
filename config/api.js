9ab80c307c4b817897e902d7357a0df8f1f12e67
gaguirr5 <77712470+gaguirr5@users.noreply.github.com>
2022-07-15 09:47:20 -0500

    updated to singular

--- config/api.js
+++ config/api.js
@@ -1,8 +1,8 @@
 module.exports = {
-    rest: {
-      defaultLimit: 25,
-      maxLimit: 100,
-      withCount: true,
-    },
+    // rest: {
+    //   defaultLimit: 25,
+    //   maxLimit: 100,
+    //   withCount: true,
+    // },
   };
   
