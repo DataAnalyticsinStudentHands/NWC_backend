b0639f723ddb50f73d03820180b66b340d71cf8a
peggylind <peggylind@gmail.com>
2022-08-11 17:51:37 -0500

    updates for staging deployment

--- config/admin.js
+++ config/admin.js
@@ -2,4 +2,7 @@ module.exports = ({env}) => ({
     auth:{
         secret: env('ADMIN_JWT_SECRET', process.env.ADMIN_JWT_SECRET),
     },
+    apiToken:{ 
+        salt: env('API_TOKEN_SALT', process.env.API_TOKEN_SALT),
+    }
 });
