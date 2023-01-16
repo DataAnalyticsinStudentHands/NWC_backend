dfd2867384e667e4f781f90cede79f3611fbb612
peggylind <peggylind@gmail.com>
2023-01-16 15:25:24 -0600

    updated Excel to csv conversion for data import

--- methods/mergeImportFiles.R
+++ methods/mergeImportFiles.R
@@ -11,7 +11,7 @@ getdata_mapped <- function(inputfile, id1, id2) {
   dir.create(name, showWarnings = FALSE)
   sheets <- readxl::excel_sheets(inputfile)
   # we will only import one of the following sheets
-  accepted = c('Basic Data', 'Racial and Ethnic Identifiers', 'Ed & Career', 'Electoral Politics', 'Leadership in Org', 'Organizational and Political', 'Role at NWC', 'Sources')
+  accepted = c('Basic Data', 'Race & Ethnicity', 'Ed & Career', 'Electoral Politics', 'Leadership in Org', 'Organizational and Political', 'Role at NWC')
   
   for (x in sheets) {
     if (!x %in% accepted ) {
@@ -20,7 +20,7 @@ getdata_mapped <- function(inputfile, id1, id2) {
     print(paste0('Creating ', x, '.csv'))
     df1 <- readxl::read_excel(inputfile, sheet = x, col_types = "text") %>%
         mutate(ID = str_pad(ID, 4, pad = "0"))
-    if (exists('mapping') & ncol(mapp_df) == 4) {
+    if (length(mapping) != 0) {
        df2 <- df1 %>%
         filter(ID == id1 | ID == id2)
     } else {
@@ -51,34 +51,33 @@ print(paste('Read files in directory: ', directory))
 files<-list.files(path=directory, pattern='xlsx', full.names = TRUE)
 mapping <- list.files(path=directory, pattern='csv', full.names = TRUE)
 
-if (exists('mapping')) {
-  print(paste('Found mapping file: ', mapping))
-  mapp_df <- read_csv(mapping)  
-} else {
-  abort("Abort. Please provide a mapping file.")
-}
-
 for (inputfile in files){
     print(paste('Starting import for file: ', inputfile))
     file <- tools::file_path_sans_ext(basename(inputfile))
-    if (exists('mapping') & ncol(mapp_df) == 4) {
-      # STEP 1: get mapping information
-      ids <- mapp_df %>% filter(str_detect(`File Name`, file))
-      # STEP 2: read Excel sheets, keep only rows according to mapping
-      import_directory = getdata_mapped(inputfile, ids$`ID Code 1`[1], ids$`ID Code 2`[1])
-      note <- paste(mapp_df[1,4], file)
+    if (length(mapping) != 0) {
+      print(paste('Found mapping file: ', mapping))
+      mapp_df <- read_csv(mapping) 
+      if(ncol(mapp_df) == 4) {
+        # STEP 1: get mapping information
+        ids <- mapp_df %>% filter(str_detect(`File Name`, file))
+        # STEP 2: read Excel sheets, keep only rows according to mapping
+        import_directory = getdata_mapped(inputfile, ids$`ID Code 1`[1], ids$`ID Code 2`[1])
+        note <- paste(mapp_df[1,4], file)
+      } else {
+        stop("Mapping file format not correct.")
+      }
     } else {
+      print('Not using a mapping file')
       import_directory = getdata_mapped(inputfile)
-      note <- paste(mapp_df[1,2], file)
     }
 
     # STEP 3: import into DB
-    print("Call import.")
-    execute_import_command = paste0('node import_participants --directory \"', import_directory, '\"', ' --note \"', note, '\" -b')
-    try(system(execute_import_command, intern = FALSE))
-    print("Call import non basic.")
-    execute_import_command = paste0('node import_participants --directory \"', import_directory, '\"', ' --note \"', note, '\" -a -e -p -r -l -o')
-    try(system(execute_import_command, intern = FALSE))
+    #print("Call import.")
+    #execute_import_command = paste0('node import_participants --directory \"', import_directory, '\"', ' --note \"', note, '\" -b')
+    #try(system(execute_import_command, intern = FALSE))
+    #print("Call import non basic.")
+    #execute_import_command = paste0('node import_participants --directory \"', import_directory, '\"', ' --note \"', note, '\" -a -e -p -r -l -o')
+    #try(system(execute_import_command, intern = FALSE))
 }
 
 
