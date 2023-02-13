f64b53d094b41fcf257c1fa855206b9611eced0f
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2023-02-13 09:49:32 -0600

    Code clean

--- methods/data_transform/education_and_career.js
+++ methods/data_transform/education_and_career.js
@@ -1,4 +1,5 @@
 const CSVToJSON = require("csvtojson");
+const { startsWith } = require("lodash");
 const _sth = require("./utility.js");
 const fs = require("fs");
 var participants = JSON.parse(fs.readFileSync("participants.json", "utf-8"));
--- methods/data_transform/electiral_politics.js
+++ methods/data_transform/electiral_politics.js
@@ -1,5 +1,4 @@
 const CSVToJSON = require("csvtojson");
-const { startsWith } = require("lodash");
 const _sth = require("./utility.js");
 const fs = require("fs");
 var participants = JSON.parse(fs.readFileSync("participants.json", "utf-8"));
@@ -8,49 +7,51 @@ async function get_electoral_politices() {
 	try {
 		const csvData = await CSVToJSON().fromFile("./data/Electoral Politics.csv");
 		let party_data = []; let commission_data = []; let suppose_party_data = [];
-        let office_hold_data = []; let office_lost_data = [];
-		csvData.forEach((e) => {
-			let keys = Object.keys(e);
-			keys.forEach((key) => {
-                if(key === "ID" || key === "Name" || startsWith(key, "...") ||key === "Notes" || e[key] == "NA"){
-                } else if (key === "Political Party Membership") {
-                    party_data.push({political_party: e[key],participant_id: parseInt(e.ID)})
-                } else if ( startsWith(key, "President's Commission")
-                            || startsWith(key, "state level Commission")
-                            || startsWith(key, "county level Commission")
-                            || startsWith(key, "city level Commission")) {
-                                commission_data.push({level_of_commission: key ,participant_id: parseInt(e.ID)})
-                } else if (startsWith(key, "Spouse")) {
-                    suppose_party_data.push({spouse_political_office: e[key],participant_id: parseInt(e.ID)})
-                } else if (startsWith(key, "Name of Political Offices Held")) {
-                    office_hold_data.push(_sth.removeNullUndefined({
-                        participant_id: parseInt(e.ID),
-                        level: e['Jurisdiction of Political Offices Held (if true for more than one category, create a new row for each)'] == 'NA' ? null : e['Jurisdiction of Political Offices Held (if true for more than one category, create a new row for each)'],
-                        start_year: e['Start Year for Political Office'] == "NA"? null : parseInt(e['Start Year for Political Office']),
-                        end_year: e['End Year for Political Office'] == 'NA' ? null : parseInt(e['End Year for Political Office']),
-                        jurisdiction_of_political_offices_held: e[key]
-                    }))
-                } else if (startsWith(key, "Name of Political Offices Sought but Lost")) {
-                    office_lost_data.push(_sth.removeNullUndefined({
-                        participant_id: parseInt(e.ID),
-                        level: ['NA','none'].includes(e['Jurisdiction of Political Offices Sought but Lost']) ? null : e['Jurisdiction of Political Offices Held (if true for more than one category, create a new row for each)'],
-                        year: e['Year of Race that was Lost'] == "NA"? null : parseInt(e['Year of Race that was Lost']),
-                        political_offices_name: e[key]
-                    }))
-                } 
-			});
-		});
+        let office_hold_data = []; let office_lost_data = []; feminist_data = [];
+        csvData.forEach((e) => {
+            let values = Object.values(e);
+            let keys = Object.keys(e);
+
+            !["NA","none"].includes(values[2]) || values[3] !== "NA"  || values[4] !== 'NA' ||values[5] !== 'NA' ? 
+            office_hold_data.push(_sth.removeNullUndefined({
+                participant_id: parseInt(e.ID),
+                level: !["NA","none"].includes(values[2]) ? values[2] : null,
+                political_offices_name: values[3] !== "NA" ? values[3] : null,
+                start_year: values[4] !== "NA" ? values[4] : null,
+                end_year: values[5] !== "NA" ? values[5] : null,
+
+            })) : null;
+            !["NA","none"].includes(values[6]) || values[7] !== "NA"  || !Number.isNaN(parseInt(values[8])) ? 
+            office_lost_data.push(_sth.removeNullUndefined({
+                participant_id: parseInt(e.ID),
+                level: !["NA","none"].includes(values[6]) ? values[6] : null,
+                political_offices_name: values[7] !== "NA" ? values[7] : null,
+                year: !Number.isNaN(parseInt(values[8])) ? parseInt(values[8]) : null,
+            })) : null;
+
+            values[9] !== "NA" ? party_data.push({political_party: values[9],participant_id: parseInt(e.ID)}) : null;
+            values[10] !== "NA" ? suppose_party_data.push({spouse_political_office: values[10],participant_id: parseInt(e.ID)}) : null;
+            values[11] == "yes" ? feminist_data.push({feminist: true,participant_id: parseInt(e.ID)}) : null;
+
+            values[12] !== "NA" ? commission_data.push({level_of_commission: keys[12],participant_id: parseInt(e.ID)}) : null;
+            values[13] !== "NA" ? commission_data.push({level_of_commission: keys[13],participant_id: parseInt(e.ID)}) : null;
+            values[14] !== "NA" ? commission_data.push({level_of_commission: keys[14],participant_id: parseInt(e.ID)}) : null;
+            values[15] !== "NA" ? commission_data.push({level_of_commission: keys[15],participant_id: parseInt(e.ID)}) : null;
+        });
         // // API commission
         participants = _sth.handleAPI(commission_data, participants, 'level_of_commission', Object.keys(participants.data)[6])
-        // API party
+        // // API party
         participants = _sth.handleAPI(party_data, participants, 'political_party', Object.keys(participants.data)[7])
-        // Component suppose party
+        // // feminist
+        feminist_data.forEach((e) => {participants.data[Object.keys(participants.data)[0]][`${e.participant_id}`].feminist = true;})
+        // // Component suppose party
         participants = _sth.handleComponent(suppose_party_data, participants, Object.keys(participants.data)[12])
         participants = _sth.pushComonent(suppose_party_data, participants, "spouse_political_office")
-        // Component political_office_hold
+
+        // // Component political_office_hold
         participants = _sth.handleComponent(office_hold_data, participants, Object.keys(participants.data)[14])
         participants = _sth.pushComonent(office_hold_data, participants, "political_office_hold")
-        // Component political_office_lost
+        // // // Component political_office_lost
         participants = _sth.handleComponent(office_lost_data, participants,Object.keys(participants.data)[13])
         participants = _sth.pushComonent(office_lost_data, participants, "political_offices_lost")
 
@@ -62,7 +63,6 @@ async function get_electoral_politices() {
             }
             console.log("participants.json has been saved.");
         });
-
 	} catch (err) {
 		console.log(err);
 	}
