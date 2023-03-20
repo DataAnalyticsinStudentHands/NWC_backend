b2e854234e6f913f2c2908d29304e700b8e4daa2
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2023-03-20 12:34:20 -0500

    Update server.js

--- config/server.js
+++ config/server.js
@@ -3,11 +3,11 @@ module.exports = ({ env }) => (
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
     //v4.6.1
-    url: 'http://localhost:1337',
-    webhooks: {
-      // Add this to not receive populated relations in webhooks
-      populateRelations: false,
-    },
+    // url: 'http://localhost:1337',
+    // webhooks: {
+    //   // Add this to not receive populated relations in webhooks
+    //   populateRelations: false,
+    // },
     //v4 optional
     app:{
       keys:env.array('APP_KEYS')
