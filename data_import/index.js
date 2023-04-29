f80ca2d46f1b450d2ad23dec3b2f56a01168ac95
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2023-04-29 13:41:45 -0500

    Update to 3.0 Version

--- data_import/index.js
+++ data_import/index.js
@@ -1,119 +1,176 @@
 const xlsx = require("xlsx");
 const fs = require("fs");
 const _ = require("lodash");
+const { toObject, difference, signId, removeNullUndefined } = require("./utils/utils.js");
+const { cleanStrapiObject, cleanStrapiObjectI, cleanStrapiArray } = require("./utils/strapiData.js");
+const { cleanSheetObject, cleanSheetObjectI, cleanSheetArray } = require("./utils/sheetData.js");
 
-const basic = require("./function/handleBasic.js");
-const basicrace = require("./function/handleBasicRace.js");
-const race = require("./function/handleRace.js");
-const educarr = require("./function/handleEdandCar.js");
-const electpol = require("./function/handleElectoralPolitics.js");
-const leadership = require("./function/handleLeadership.js");
-const organization = require("./function/handleOrganizational.js");
-const nwcrole = require("./function/handleNWCRole.js");
-const position = require("./function/handlePosition.js");
+const workbook = xlsx.readFile(process.argv[2]);
+const sheets = workbook.SheetNames.reduce((acc, sheetName) => {
+    acc[sheetName] = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
+    return acc;
+}, {});
 
-const handleData = async (data, handler, params) => {
-	if (data) {
-		return await handler(data, ...params);
-	}
-	return null;
-};
+const jsonData = JSON.parse(fs.readFileSync(process.argv[3]), "utf8");
+const config = JSON.parse(fs.readFileSync("./config.json"), "utf8");
 
-async function main() {
+function OO (key){
+    const {pk, lookup,sheet, api} = config[key];
+    const sheetData = sheets[sheet];
+    const strapiData = jsonData.data[api];
 
-	const file = fs.readFileSync(process.argv[2]);
-	const workbook = xlsx.read(file, { type: "array" });
-	const sheets = {};
-	workbook.SheetNames.forEach((sheet) => {
-		sheets[sheet] = xlsx.utils.sheet_to_json(workbook.Sheets[sheet]);
-	});
+    const newStrapiData = cleanStrapiObject(Object.values(strapiData),lookup, pk);
+    const newSheetData = cleanSheetObject(sheetData, lookup, pk);
 
-	const dataContent = JSON.parse(fs.readFileSync(process.argv[3], "utf8"));
+    const diff = difference(
+        Object.values(newSheetData),
+        Object.values(newStrapiData),
+        _.isEqual
+    );
+    return {[api]:toObject(diff,pk)};
+}
+function OOI (key){
+    const {pk, lookup,sheet, api} = config[key];
+    const sheetData = sheets[sheet];
+	const strapiData = jsonData.data[api];
 
-	const [
-		basicData,
-		basicRaceData,
-		raceData,
-		edAndCareerData,
-		electoralPoliticsData,
-		leadershipData,
-		organizationalData,
-		roleData,
-		positionData,
-	] = await Promise.all([
-		handleData(
-            sheets["Basic Data"], basic.handleBasicData, [
-			dataContent.data["api::nwc-participant.nwc-participant"],
-			dataContent.data["api::resident-in-1977.resident-in-1977"],
-		]),
-		handleData(
-			sheets["Race & Ethnicity--Reg Forms"], basicrace.handleBasicRaceData,[
-				dataContent.data["api::basic-race.basic-race"]
-			]
-		),
-		handleData(sheets["Race & Ethnicity--Expanded"], race.handleRaceData, [
-			dataContent.data["api::race.race"],
-		]),
-		handleData(sheets["Ed & Career"], educarr.handleEdandCareerData, [
-			dataContent.data["api::nwc-participant.nwc-participant"],
-			dataContent.data["api::data-education.data-education"],
-			dataContent.data["api::data-career.data-career"],
-			dataContent.data["api::data-spouse-career.data-spouse-career"],
-		]),
-		handleData(
-			sheets["Electoral Politics"], electpol.handleElectoralPoliticsData,[
-				dataContent.data["api::nwc-participant.nwc-participant"],
-				dataContent.data["api::data-political-office-held.data-political-office-held"],
-				dataContent.data["api::data-political-office-lost.data-political-office-lost"],
-				dataContent.data["api::data-spouse-political-office.data-spouse-political-office"],
-			]
-		),
-		handleData(
-			sheets["Leadership in Org"],
-			leadership.handleLeadershipData,[
-				dataContent.data["api::data-leadership-in-organization.data-leadership-in-organization"],
-			]
-		),
-		handleData(
-			sheets["Organizational & Political"], organization.handleOrganizationalData,[
-				dataContent.data["api::organizational-and-political.organizational-and-political"],
-			]
-		),
-		handleData(sheets["Role at NWC"], nwcrole.handleNWCRoleData, [
-			dataContent.data["api::nwc-participant.nwc-participant"],
-			dataContent.data["api::nwc-role.nwc-role"],
-		]),
-		handleData(sheets["Position on Planks"], position.handlePositionData, [
-			dataContent.data["api::nwc-plank.nwc-plank"],
-		]),
-	]);
+	const newStrapiData = cleanStrapiObjectI(Object.values(strapiData),lookup, pk);
+	const newSheetData = cleanSheetObjectI(sheetData, lookup, pk);
 
-	const mergedData = _.merge(
-		{},
-		basicData,
-		basicRaceData,
-		raceData,
-		edAndCareerData,
-		electoralPoliticsData,
-		leadershipData,
-		organizationalData,
-		roleData,
-		positionData
+	const diff = difference(
+		Object.values(newSheetData),
+		Object.values(newStrapiData),
+		_.isEqual
 	);
+    const newObj = signId(diff,strapiData, lookup, pk);
+    return {[api]:newObj};
+}
 
-	fs.writeFileSync(
-		"data.json",
+function AA(key){
+    switch (key){
+        
+        case 'education_edu':{
+            const {pk, lookup_gard, lookup_undergrad,sheet, api} = config[key];
+            const sheetData = sheets[sheet];
+            const strapiData = jsonData.data[api];
+            const maxId = Math.max(...Object.keys(strapiData).map(Number));
+        
+            const newStrapiData = cleanStrapiArray(Object.values(strapiData), lookup_gard, pk);
+            const newSheetData = [
+                ...cleanSheetArray(sheetData, lookup_gard, pk), 
+                ...cleanSheetArray(sheetData, lookup_undergrad, pk)
+            ];        
+            const diff = difference(
+                newSheetData, 
+                newStrapiData, 
+                _.isEqual
+            );
+        
+            const newObj = {};
+            diff.forEach((item, index) => {
+                newObj[index + maxId + 1] = {
+                    id: index + maxId + 1,
+                    ...item
+                }
+            });
+            return {[api]:newObj};
+        }
+        // Residence should not mix in Basci Data.
+        case 'residence_in_1977':{
+            const {pk, lookup,sheet, api} = config[key];
+
+            const sheetData = sheets[sheet];
+            const strapiData = jsonData.data[api];
+        
+            const newStrapiData = cleanStrapiArray(Object.values(strapiData), lookup, pk);
+            const newSheetData = {};
+            cleanSheetArray(sheetData, lookup, pk).forEach((item) => {
+                newSheetData[item.residence_in_1977]
+                ? newSheetData[item.residence_in_1977].participants.push(item.participant_id)
+                : newSheetData[item.residence_in_1977] = removeNullUndefined({
+                    participants: [item.participant_id],
+                    residence_in_1977: item.residence_in_1977,
+                    total_population: item.total_population,
+                    median_household_income: item.median_household_income,
+                })
+            });
+        
+            const diff = difference(
+                Object.values(newSheetData), 
+                newStrapiData, 
+                _.isEqual
+            );
+
+            const newObj = signId(diff,strapiData, lookup, pk);
+            return {[api]:newObj};
+
+        }
+        default:{
+            const {pk, lookup,sheet, api} = config[key];
+            const sheetData = sheets[sheet];
+            const strapiData = jsonData.data[api];
+            const maxId = Math.max(...Object.keys(strapiData).map(Number));
+        
+            const newStrapiData = cleanStrapiArray(Object.values(strapiData),lookup, pk);
+            const newSheetData = cleanSheetArray(sheetData, lookup, pk);
+        
+            const diff = difference(
+                newSheetData, 
+                newStrapiData, 
+                _.isEqual
+            );
+        
+            const newObj = {};
+            diff.forEach((item, index) => {
+                newObj[index + maxId + 1] = {
+                    id: index + maxId + 1,
+                    ...item
+                }
+            });
+            return {[api]:newObj};
+        }
+    }
+}
+
+async function preFlight() {
+    // OO
+    const participant = OO('participant')
+    const education_participant = OO('education_participant')
+    const politics_participant = OO('politics_participant')
+    const role_participant = OO('role_participant')
+
+    // OOI
+    const basic_race = OOI('basic_race')
+    const race = OOI('race')
+    const organization_and_political = OOI('organization_and_political')
+    const role = OOI('role')
+    const plank = OOI('plank')
+    // AA
+    const residence_in_1977 = AA('residence_in_1977')
+    const education_edu = AA('education_edu')
+    const education_spouse_career = AA('education_spouse_career')
+    const eudcation_career = AA('eudcation_career')
+    const politics_office_hold = AA('politics_office_hold')
+    const politics_office_lost = AA('politics_office_lost')
+    const politics_office_spouse = AA('politics_office_spouse')
+    const leadership_in_org = AA('leadership_in_org')
+
+
+
+    const merged = _.merge(
+        participant, education_participant, politics_participant, role_participant,
+        basic_race, race, organization_and_political, role, plank,
+        residence_in_1977, education_edu, education_spouse_career, eudcation_career, politics_office_hold, politics_office_lost, politics_office_spouse, leadership_in_org
+    )
+
+    fs.writeFileSync(
+		"newData.json",
 		JSON.stringify({
 			version: 2,
-			data: mergedData,
+			data: merged,
 		}),
 		"utf-8"
 	);
 }
-main()
-	.then(() => {
-		console.log("done");
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+
+preFlight()
