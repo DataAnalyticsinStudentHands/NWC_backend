26c5e33d301cce6d66901cc5753bad28d5f7fa50
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2023-06-05 13:33:41 +0800

    Update preflight.js
    Quick FIX

--- src/plugins/preflight-check/admin/src/api/preflight.js
+++ src/plugins/preflight-check/admin/src/api/preflight.js
@@ -1,32 +1,34 @@
 import axios from "axios";
-const token = process.env.STRAPI_TOKEN;
+
+const token = process.env.STRAPI_WEBTOKEN
 const header = {
   headers: {
     Authorization: `Bearer ${token}`,
   },
 };
+
 const preflightRequests = {
   getData: async () => {
-    const exportURL =`/api/import-export-entries/content/export/contentTypes`;
+    const exportURL =`https://dash.cs.uh.edu/api/import-export-entries/content/export/contentTypes`;
     const params = {
       slug: "api::nwc-participant.nwc-participant",
       exportFormat: "json-v2",
       deepness: 3,
     };
 
-    const results = await axios.post(exportURL, params, header);
+    const results = await axios.post(exportURL, params);
     return JSON.parse(results.data.data);
   },
 
   importData: async (dataContent) => {
-    const importURL ="/api/import-export-entries/content/import";
+    const importURL =`https://dash.cs.uh.edu/api/import-export-entries/content/import`;
     const data = {
       slug: "api::nwc-participant.nwc-participant",
       data: JSON.stringify(dataContent),
       format: "json",
     };
 
-    axios.post(importURL, data, header);
+    axios.post(importURL, data);
   },
 };
 export default preflightRequests;
