55938e5208e4f34e0d8a020937c8b723a4c31633
Thang Nguyen <nguyenn.thang@yahoo.com>
2023-09-06 02:38:32 -0500

    Oral histories schema added

---
+++ src/api/content-oral-history/services/content-oral-history.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * content-oral-history service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::content-oral-history.content-oral-history');
