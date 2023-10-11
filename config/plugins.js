9baae8322bb844d9e0c030df103ad94d04788501
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2023-10-11 11:22:56 -0500

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -12,7 +12,7 @@ module.exports = {
     enabled: true,
   },
   email: {
-    provider: "sendmail",
+    provider: "nodemailer",
     providerOptions: {
       sendmail: true,
       newline: "unix",
