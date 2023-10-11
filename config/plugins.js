313e48d39f65fcf662dc80e0debcef9b9dce3916
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2023-10-11 11:15:56 -0500

    Update plugins.js
    Debug Email

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,23 @@
-module.exports =  {
-    // ...
-    'email-service': {
-      enabled: true,
-      resolve: './src/plugins/email-service'
+module.exports = {
+  // ...
+  "email-service": {
+    enabled: true,
+    resolve: "./src/plugins/email-service",
+  },
+  "preflight-check": {
+    enabled: true,
+    resolve: "./src/plugins/preflight-check",
+  },
+  "import-export-entries": {
+    enabled: true,
+  },
+  email: {
+    provider: "sendmail",
+    providerOptions: {
+      sendmail: true,
+      newline: "unix",
+      path: "/usr/sbin/sendmail",
     },
-    'preflight-check': {
-      enabled: true,
-      resolve: './src/plugins/preflight-check'
-    },
-    'import-export-entries': {
-      enabled: true,
-      // config: {
-      //   // See `Config` section.
-      //   serverPublicHostname: 'https://dash.cs.uh.edu',
-      // },
-    },
-    // ...
-  }
+  },
+  // ...
+};
