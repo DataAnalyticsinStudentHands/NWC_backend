424896f31cc1ca457381e19345627d8e33c30d72
Thang Nguyen <nguyenn.thang@yahoo.com>
2025-01-30 02:40:31 -0600

    - Added new content-type collection content-homehighlights - Removed content-home-carousel collectino

--- src/api/content-home-carousel/controllers/content-home-carousel.js
+++
@@ -1,2 +0,0 @@
-const { createCoreController } = require('@strapi/strapi').factories;
-module.exports = createCoreController('api::content-home-carousel.content-home-carousel')
---
+++ src/api/content-home-highlight/controllers/content-home-highlight.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * content-home-highlight controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::content-home-highlight.content-home-highlight');
