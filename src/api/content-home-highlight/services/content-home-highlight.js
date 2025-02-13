d57a4d6f67cb09b2476e95f77662a1c667242db3
Thang Nguyen <89552284+ThangNg98@users.noreply.github.com>
2025-02-13 10:09:42 -0600

    Carousel Content Cleanup (#78)
    * - Added new content-type collection content-homehighlights
    - Removed content-home-carousel collectino
    
    * - Fixed capitalization in display name Content -> content

---
+++ src/api/content-home-highlight/services/content-home-highlight.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * content-home-highlight service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::content-home-highlight.content-home-highlight');
