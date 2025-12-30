3b956ba8e5ceb8025a5d705cf398cbb7d3d85c76
Peggy Lindner <peggylind@gmail.com>
2025-12-30 11:01:15 +0100

    added apis for researcher, student and educator inquiry forms

---
+++ src/api/form-inquiryresearcher/routes/form-inquiryresearcher.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * form-inquiryresearcher router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::form-inquiryresearcher.form-inquiryresearcher');
---
+++ src/api/form-inquirystudent/routes/form-inquirystudent.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * form-inquirystudent router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::form-inquirystudent.form-inquirystudent');
