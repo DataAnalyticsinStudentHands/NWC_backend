ddbf43739794315080b627bc5505f9a04fea8a86
Peggy Lindner <peggylind@gmail.com>
2026-01-12 15:42:00 -0600

    Adding new collection for archivist inquiry form

---
+++ src/api/form-inquiryarchivist/routes/form-inquiryarchivist.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * form-inquiryarchivist router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::form-inquiryarchivist.form-inquiryarchivist');
---
+++ src/api/form-inquiryarchivist/services/form-inquiryarchivist.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * form-inquiryarchivist service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::form-inquiryarchivist.form-inquiryarchivist');
